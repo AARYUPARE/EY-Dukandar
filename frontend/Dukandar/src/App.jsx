bec0ee0a98c5987dcd4f7bc54366533200ed0463
AARYUPARE <aaryupare2006@gmail.com>
2025-12-06 16:13:50 +0530

    combine

--- frontend/Dukandar/src/App.jsx
+++ frontend/Dukandar/src/App.jsx
@@ -2,10 +2,9 @@ import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import './App.css'
 
 const App = () => {
-
-
 
   return (
     <>
