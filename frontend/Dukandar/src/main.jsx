9b8589f01dbd952a96e03317bd5d2d49e0244f06
AARYUPARE <aaryupare2006@gmail.com>
2025-12-06 15:58:16 +0530

    bootstrap installed

--- frontend/Dukandar/src/main.jsx
+++ frontend/Dukandar/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"></link>
+import './App.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
